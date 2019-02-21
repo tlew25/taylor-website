1f8ab18292adb272c1e72c8709a527407f845b6e
Taylor Lewis <tlew@Taylors-MacBook-Pro.local>
2019-02-21 10:30:35 -0600

    major updates

---
+++ assets/js/firebase.js
@@ -0,0 +1,57 @@
+//FIREBASE JS FOR CLIENT STORAGE
+var config = {
+    apiKey: "AIzaSyAUrgeA3yp3DEXUUwUvEbyK4m6EcLMKvtI",
+    authDomain: "contactform2393.firebaseapp.com",
+    databaseURL: "https://contactform2393.firebaseio.com",
+    projectId: "contactform2393",
+    storageBucket: "contactform2393.appspot.com",
+    messagingSenderId: "230097572926"
+};
+firebase.initializeApp(config);
+
+
+var messagesRef = firebase.database().ref('comments');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    // Get values
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var comments = getInputVal('comments');
+
+    // Save message
+    saveMessage(name, email, phone, comments);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function () {
+        document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+
+    // Clear form
+    document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, phone, comments) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        phone: phone,
+        comments: comments
+    });
+}
